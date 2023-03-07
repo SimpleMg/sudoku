9c242c573aee511dd61db3549f89ffd44d20e0bf
SimpleMg <117079680+SimpleMg@users.noreply.github.com>
2023-03-07 18:09:25 +0100

    Update lib_sudoku.h

--- lib_sudoku/lib_sudoku.h
+++ lib_sudoku/lib_sudoku.h
@@ -1,10 +1,17 @@
 #ifndef LIB_SUDOKU
 #define LIB_SUDOKU
-
 #define NBRE_LIGNE 9
 #define NBRE_COLONNE 9
+#define VRAI 1
+#define FAUX 0
+
+typedef int BOOL;
 
 void afficher_grille(int **board);
+BOOL horizontal_check(int **board);
+BOOL vertical_check(int **board);
+BOOL carre_check(int **board);
+int** resolv_sudoku(int **board);
 
 
 #endif
