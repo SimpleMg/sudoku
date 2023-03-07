dff4fccface6de1b8c123f25495268679deab0fb
SimpleMg <117079680+SimpleMg@users.noreply.github.com>
2023-03-07 15:57:44 +0100

    Update sudoku.c

--- sudoku.c
+++ sudoku.c
@@ -3,16 +3,65 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include "lib/biblio.h"
+#include "Biblio_C/biblio.h"
+#include "lib_sudoku/lib_sudoku.h"
+
+#define nbre_ligne 9
+#define nbre_colonne 9
+
 
 int main(int argc, char **argv)
 {
-    int buffer[1024];
-    int fd = open(argv[1], 'r');
+    if(argc != 3)
+    {
+        mg_putstr("Merci de renseigner le fichier txt exemple: ./a.out 3 exemple.txt\n");
+        return 0;
+    }
+    /*RECUPERATION DES DONNEES DU FICHIER TXT*/
+    char buffer[1024];
+    int fd = open(argv[2], O_RDONLY);
     int size = read(fd, buffer, 1024);
-
-    write(1, buffer, size);
     close(fd);
-
-    return 0;
+    mg_putchar('\n');
+    /*CREATION DE LA GRILLE*/
+    //Allocation de la mémoire
+    int **board = (int**)malloc(nbre_ligne * sizeof(int*));
+    if(board == NULL)
+    {
+        mg_putstr("Mauvaise allocation mémoire\n");
+        return 0;
+    }
+    int i, j, i_buffer;
+    i = 0;
+    while(i < nbre_ligne)
+    {
+        board[i] = (int*)malloc(nbre_ligne * sizeof(int));
+        if(board[i] == NULL)
+        {   
+            mg_putstr("Mauvaise allocation mémoire\n");
+            return 0;
+        }
+        i++;
+    }
+    //remplissage de la grille
+    i = 0;
+    j = 0;
+    i_buffer = 0;
+    while(i < nbre_ligne)
+    {
+        while(j < nbre_colonne)
+        {
+            if(buffer[i_buffer] >= '0' && buffer[i_buffer] <= '9')
+            {
+                board[i][j] = buffer[i_buffer] - '0';
+            }
+            j++;
+            i_buffer++;
+        }
+        i++;
+        j = 0;
+    }
+    //affiche de la grille
+    afficher_grille(board);
+    return 1;
 }
