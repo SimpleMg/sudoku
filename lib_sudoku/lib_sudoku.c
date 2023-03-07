b4dc74cfb83496afa434d89ba29329f1711b608b
SimpleMg <117079680+SimpleMg@users.noreply.github.com>
2023-03-07 16:15:06 +0100

    Update lib_sudoku.c

--- lib_sudoku/lib_sudoku.c
+++ lib_sudoku/lib_sudoku.c
@@ -7,25 +7,20 @@ void afficher_grille(int **board)
 {
     int i = 0;
     int j = 0;
-    while(i < nbre_ligne)
+    while(i < NBRE_LIGNE)
     {
-        if(i % 3 == 0 && i != nbre_ligne && i != 0)
+        if(i % 3 == 0 && i != NBRE_LIGNE && i != 0)
         {
             mg_putstr("--------------------\n");
         }
-        while(j < nbre_colonne)
+        while(j < NBRE_COLONNE)
         {
-            if((j + 1) % 3 == 0 && j != nbre_colonne - 1)
+            if((j + 1) % 3 == 0 && j != NBRE_COLONNE - 1)
             {
-                mg_putchar(board[i][j] + '0');
-                mg_putchar(' ');
                 mg_putchar('|');
             }
-            else
-            {
                 mg_putchar(board[i][j] + '0');
                 mg_putchar(' ');
-            }
             j++;
         }
         mg_putchar('\n');
