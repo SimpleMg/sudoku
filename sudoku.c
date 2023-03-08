a5d0131f4e29ba184c58a82ac9442ca435851bb4
SimpleMg <117079680+SimpleMg@users.noreply.github.com>
2023-03-08 13:58:06 +0100

    Update sudoku.c

--- sudoku.c
+++ sudoku.c
@@ -6,62 +6,72 @@
 #include "Biblio_C/biblio.h"
 #include "lib_sudoku/lib_sudoku.h"
 
-
 int main(int argc, char **argv)
 {
     /*VERIFIE SI L'UTILISATEUR PASSE BIEN LE FICHIER TEXT EN ARGUMENT*/
-    if(argc != 3)
+    if (argc < 2)
     {
         mg_putstr("Merci de renseigner le fichier txt exemple: ./a.out 3 exemple.txt\n");
         return 0;
     }
     /*RECUPERATION DES DONNEES DU FICHIER TXT*/
-    char buffer[1024];
-    int fd = open(argv[2], O_RDONLY);
-    int size = read(fd, buffer, 1024);
+    char buffer[200];
+    int fd = open(argv[1], O_RDONLY);
+    read(fd, buffer, 200);
     close(fd);
     /*CREATION DE LA GRILLE*/
-    //Allocation de la mémoire
-    int **board = (int**)malloc(NBRE_LIGNE * sizeof(int*));
-    if(board == NULL)
+    // Allocation de la mémoire
+    int **board = (int **)malloc(NBRE_LIGNE * sizeof(int *));
+    if (board == NULL)
     {
         mg_putstr("Mauvaise allocation mémoire\n");
         return 0;
     }
     int i, j, i_buffer;
     i = 0;
-    while(i < NBRE_LIGNE)
+    while (i < NBRE_LIGNE)
     {
-        board[i] = (int*)malloc(NBRE_LIGNE * sizeof(int));
-        if(board[i] == NULL)
-        {   
+        board[i] = (int *)malloc(NBRE_LIGNE * sizeof(int));
+        if (board[i] == NULL)
+        {
             mg_putstr("Mauvaise allocation mémoire\n");
             return 0;
         }
         i++;
     }
-    //remplissage de la grille
+    // remplissage de la grille
     i = 0;
     j = 0;
     i_buffer = 0;
-    while(i < NBRE_LIGNE)
+    while (i < NBRE_LIGNE)
     {
-        while(j <= NBRE_COLONNE)
+        while (j < NBRE_COLONNE)
         {
-            if(buffer[i_buffer] >= '0' && buffer[i_buffer] <= '9')
+            if (buffer[i_buffer] >= '0' && buffer[i_buffer] <= '9')
             {
                 board[i][j] = buffer[i_buffer] - '0';
+                j++;
             }
-            j++;
             i_buffer++;
         }
         i++;
         j = 0;
     }
-    //affiche de la grille
+
+    // affiche de la grille
     afficher_grille(board);
-    /*RESOLUTION DE LA GRILLE*/
+    /*
+    RESOLUTION DE LA GRILLE
     board = resolv_sudoku(board);
     afficher_grille(board);
+    */
     return 1;
 }
+
+/*
+resoudre le probleme memoire
+suite:
+faire le debut de l'algo de resolution
+chaque ligne chaque colonne et chaque carre de 3 les chiffres qui manquent.
+
+*/
